a288574b345b0cbca26a4514c186d8aa2265005e
Am99473 <56793867+Am99473@users.noreply.github.com>
2024-12-29 01:17:38 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2013,6 +2013,6 @@ def value_update(*dropdown_values):
           Slabs, Rebates
       ]
 
-app.run_server(debug=False, host="0.0.0.0", port=port)
+app.run_server(debug=False, host="0.0.0.0")
 
 #app.run_server(debug=True)
